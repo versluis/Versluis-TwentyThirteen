26954aa5547d27b8d66b920897ee8630df0596eb
Jay Versluis <versluis2000@yahoo.com>
2018-09-10 15:29:26 -0400

    added links to podcasts on iTunes

--- includes/podcast-badge.php
+++ includes/podcast-badge.php
@@ -3,18 +3,28 @@
 // Apple Podcast Badge functions
 // since @1.8
 
-function versluis2013_add_content ($content){
+function versluis2013_add_badge ($content){
+	
+	$badge = get_stylesheet_directory_uri() . "/images/Apple-Podcasts-Badge.png";
 	
 	if (in_category ('Podcast')) {
-		$after = "It's a Podcast";
-	} else {
-		$after = "This is not a Podcast";
-	}
+		
+		// 3D Podcast
+		$after = "Catch this episode on my 3D Podcast:<br><a href='https://itunes.apple.com/us/podcast/jays-3d-podcast/id1433986629' target='_blank'><img id='apple-badge' src='".$badge . "'></a>";
 	
-	$before = "This comes before the real content.";
-	$fullcontent = $before . $content . $after;
-	return $fullcontent;
+	} else if (in_category ('Storyist Podcast')) {
+		
+		// Storyist Podcast
+		$after = "Catch this episode on my Storyist Podcast:<br><a href='https://itunes.apple.com/us/podcast/the-storyist-podcast/id1434351506' target='_blank'><img id='apple-badge' src='".$badge . "'></a>";
+	} else {
+		
+		// it's not a Podcast
+		$after = '';	
+	}
+
+	$content = $content . $after;
+	return $content;
 }
-add_filter ('the_content', 'versluis2013_add_content');
+add_filter ('the_content', 'versluis2013_add_badge');
 
 ?>
