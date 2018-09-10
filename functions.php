40eaffba7b1fe4996f0485277c78e0232cb46867
Jay Versluis <versluis2000@yahoo.com>
2018-09-10 13:22:36 -0400

    synced files from live version

--- functions.php
+++ functions.php
@@ -153,40 +153,52 @@ return $existing_mimes;
 }
 add_filter('upload_mimes', 'allow_brush_file_uploads');
 
-// output some text as part of the header
-// for example, add advertising
-function writeSomeText () {
-	// echo "Hello from my new header function.";
-	$words = howManyWords();
-	echo "Total words on the site: $words";
-}
-add_action('wp_head', 'writeSomeText');
+// extract JetPack pageviews
+// courtesy of Topher: http://wpgr.org/2013/03/02/rendering-jetpack-stats/
 
-// find out how many words in all posts
-function howManyWords () {
+function get_page_views($post_id) {
+
+	if (function_exists('stats_get_csv')) {
 	
-// define a variable
-$totalWords = 0;
+		$args = array('days'=>-1, 'limit'=>-1, 'post_id'=>$post_id);
+		$result = stats_get_csv('postviews', $args);
+		$views = $result[0]['views'];
 
-// grab all published posts
-$query = new WP_Query( array( 'post_status' => 'publish' ) );
+	} else {
 
-// loop through all published posts
-if ($query -> have_posts()) {
-	while ($query->have_posts()) {
-		$query->the_post();
-		
-		// remove HTML tags, leaving plain words
-		$text = trim (strip_tags(get_the_content()));
-		
-        // count spaces in each post
-		$wordCount = substr_count("$text ", ' ');
-		
-		// and add them to our totalWords variable
-		$totalWords = $totalWords + $wordCount;
+		$views = 0;
+
 	}
+	return number_format_i18n($views);
 }
 
-// return the total word count
-return $totalWords;
+/* Word Count functions */
+
+function guru_getWordCountFromPosts () {
+	
+	$count = 0;
+	$posts = get_posts( array(
+			'numberposts' => -1,
+			'post_type' => 'any'
+	));
+	
+	foreach( $posts as $post ) {
+		$count += str_word_count( strip_tags( get_post_field( 'post_content', $post->ID )));
+	}
+	$num =  number_format_i18n( $count );
+	
+	return $count;
+}
+
+function guru_getWordCountCommentsCurrentUser() {
+	
+	$count = 0;
+	$user_id = get_current_user_id();
+	$comments = get_comments( array(
+			'user_id' => $user_id
+	));
+	foreach( $comments as $comment ) {
+		$count += str_word_count( $comment->comment_content );
+	}
+	return $count;
 }
