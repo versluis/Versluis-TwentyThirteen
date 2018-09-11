663afc1f393845ba1a7b10f78354b26a6cc871b9
Jay Versluis <versluis2000@yahoo.com>
2018-09-10 21:40:28 -0400

    updated 3D Podcast category

--- includes/podcast-badge.php
+++ includes/podcast-badge.php
@@ -7,7 +7,7 @@ function versluis2013_add_badge ($content){
 	
 	$badge = get_stylesheet_directory_uri() . "/images/Apple-Podcasts-Badge.png";
 	
-	if (in_category ('Podcast')) {
+	if (in_category ('3D Podcast')) {
 		
 		// 3D Podcast
 		$after = "Catch this episode on my 3D Podcast:<br><a href='https://itunes.apple.com/us/podcast/jays-3d-podcast/id1433986629' target='_blank'><img id='apple-badge' src='".$badge . "'></a>";
@@ -16,6 +16,7 @@ function versluis2013_add_badge ($content){
 		
 		// Storyist Podcast
 		$after = "Catch this episode on my Storyist Podcast:<br><a href='https://itunes.apple.com/us/podcast/the-storyist-podcast/id1434351506' target='_blank'><img id='apple-badge' src='".$badge . "'></a>";
+		
 	} else {
 		
 		// it's not a Podcast
