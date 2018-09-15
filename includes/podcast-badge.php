69a25d124a7af100538ffd2944b0c6708812a680
Jay Versluis <versluis2000@yahoo.com>
2018-09-15 00:34:02 -0400

    added Stitcher Badge

--- includes/podcast-badge.php
+++ includes/podcast-badge.php
@@ -5,17 +5,26 @@
 
 function versluis2013_add_badge ($content){
 	
-	$badge = get_stylesheet_directory_uri() . "/images/Apple-Podcasts-Badge.png";
+	$appleBadge = get_stylesheet_directory_uri() . "/images/Apple-Podcasts-Badge.png";
+	$stitcherBadge = get_stylesheet_directory_uri() . "/images/Stitcher-Badge.png";
 	
 	if (in_category ('3D Podcast')) {
 		
 		// 3D Podcast
-		$after = "Catch this episode on my 3D Podcast:<br><a href='https://itunes.apple.com/us/podcast/jays-3d-podcast/id1433986629' target='_blank'><img id='apple-badge' src='".$badge . "'></a>";
+		$apple = "Catch this episode on my 3D Podcast:<br><a href='https://itunes.apple.com/us/podcast/jays-3d-podcast/id1433986629' target='_blank'><img id='apple-badge' src='".$appleBadge . "'></a>";
+		
+		$stitcher = "<br><a href='https://www.stitcher.com/s?fid=228524&refid=stpr' target='_blank'><img id='stitcher-badge' src='".$stitcherBadge . "'></a>";
+		
+		$after = $apple . $stitcher;
 	
 	} else if (in_category ('Storyist Podcast')) {
 		
 		// Storyist Podcast
-		$after = "Catch this episode on my Storyist Podcast:<br><a href='https://itunes.apple.com/us/podcast/the-storyist-podcast/id1434351506' target='_blank'><img id='apple-badge' src='".$badge . "'></a>";
+		$apple = "Catch this episode on my Storyist Podcast:<br><a href='https://itunes.apple.com/us/podcast/the-storyist-podcast/id1434351506' target='_blank'><img id='apple-badge' src='".$appleBadge . "'></a>";
+		
+		$stitcher = "<br><a href='https://www.stitcher.com/s?fid=228526&refid=stpr' target='_blank'><img id='stitcher-badge' src='".$stitcherBadge . "'></a>";
+		
+		$after = $apple . $stitcher;
 		
 	} else {
 		
