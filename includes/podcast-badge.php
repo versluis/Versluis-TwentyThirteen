2f67341387dfccec3341b31b5237ee3cf1175731
Jay Versluis <versluis2000@yahoo.com>
2018-09-20 00:07:20 -0400

    added YouTube Badge to serial categories

--- includes/podcast-badge.php
+++ includes/podcast-badge.php
@@ -7,6 +7,7 @@ function versluis2013_add_badge ($content){
 	
 	$appleBadge = get_stylesheet_directory_uri() . "/images/Apple-Podcasts-Badge.png";
 	$stitcherBadge = get_stylesheet_directory_uri() . "/images/Stitcher-Badge.png";
+	$youtubeBadge = get_stylesheet_directory_uri() . "/images/YouTube-Badge.png";
 	
 	if (in_category ('3D Podcast')) {
 		
@@ -20,11 +21,24 @@ function versluis2013_add_badge ($content){
 	} else if (in_category ('Storyist Podcast')) {
 		
 		// Storyist Podcast
+		$youtube = "Watch the full course in one convenient playlist:<br><a href='https://www.youtube.com/playlist?list=PLPcx_LSSGfZcwUnfc9kcXeQ1GDvCnSLgm' target='_blank'><img id='youtube-badge' src='".$youtubeBadge."'></a>";
+		
 		$apple = "Catch this episode on my Storyist Podcast:<br><a href='https://itunes.apple.com/us/podcast/the-storyist-podcast/id1434351506' target='_blank'><img id='apple-badge' src='".$appleBadge . "'></a>";
 		
 		$stitcher = "<br><a href='https://www.stitcher.com/s?fid=228526&refid=stpr' target='_blank'><img id='stitcher-badge' src='".$stitcherBadge . "'></a>";
 		
-		$after = $apple . $stitcher;
+		$after = $youtube . $apple . $stitcher;
+		
+	} else if (in_category ('DAZ Studio 101')) {
+		
+		// DAZ Studio 101 Podcast
+		$youtube = "Watch the full course in one convenient playlist:<br><a href='https://www.youtube.com/playlist?list=PLPcx_LSSGfZeCEvFO0spfYc3DzjpJyIzY' target='_blank'><img id='youtube-badge' src='".$youtubeBadge."'></a>";
+		
+		$apple = "Catch this episode on my DAZ Studio 101 Podcast:<br><a href='https://itunes.apple.com/us/podcast/the-storyist-podcast/id1434351506' target='_blank'><img id='apple-badge' src='".$appleBadge . "'></a>";
+		
+		$stitcher = "<br><a href='https://www.stitcher.com/s?fid=228526&refid=stpr' target='_blank'><img id='stitcher-badge' src='".$stitcherBadge . "'></a>";
+		
+		$after = $youtube . $apple . $stitcher;
 		
 	} else {
 		
