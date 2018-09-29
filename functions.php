3fac7929865a3ff5d5369357f2e7368de7c02182
Jay Versluis <versluis2000@yahoo.com>
2018-09-29 00:08:34 -0400

    added word count functions (again)

--- functions.php
+++ functions.php
@@ -14,3 +14,32 @@ function allow_brush_file_uploads ( $existing_mimes=array() ) {
 }
 add_filter('upload_mimes', 'allow_brush_file_uploads');
 
+/* Word Count functions */
+function guru_getWordCountFromPosts () {
+	
+	$count = 0;
+	$posts = get_posts( array(
+			'numberposts' => -1,
+			'post_type' => 'any'
+	));
+	
+	foreach( $posts as $post ) {
+		$count += str_word_count( strip_tags( get_post_field( 'post_content', $post->ID )));
+	}
+	$num =  number_format_i18n( $count );
+	
+	return $count;
+}
+
+function guru_getWordCountCommentsCurrentUser() {
+	
+	$count = 0;
+	$user_id = get_current_user_id();
+	$comments = get_comments( array(
+			'user_id' => $user_id
+	));
+	foreach( $comments as $comment ) {
+		$count += str_word_count( $comment->comment_content );
+	}
+	return $count;
+}
