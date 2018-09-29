8dc301c2e68742642d554b79eca14742a1a09b1d
Jay Versluis <versluis2000@yahoo.com>
2018-09-29 00:08:59 -0400

    added All Articles template

--- page-all-articles.php
+++ page-all-articles.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
+ * TOC - All Articles - versluis.com
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages and that other
@@ -10,7 +10,7 @@
  * @subpackage Twenty_Thirteen
  * @since Twenty Thirteen 1.0
  
-  Template Name: All My Articles
+  Template Name: TOC - All My Articles
  
  */
 
@@ -34,47 +34,55 @@ get_header(); ?>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
-						<?php the_content(); ?>
-                        
-                        <?php 
-						// show annual post archives
-						// https://codex.wordpress.org/Template_Tags/wp_get_archives
-						echo "<h3>Annual Summary</h3>";
-						
-						$args = array(
-							'type'            => 'yearly',
-							'limit'           => '',
-							'format'          => 'html', 
-							'before'          => '',
-							'after'           => '',
-							'show_post_count' => true,
-							'echo'            => 1,
-							'order'           => 'DESC'
-						);
-						wp_get_archives( $args ); 
-						// end of annual archives 
-						
-						// show a list of all articles
-						// https://codex.wordpress.org/Function_Reference/wp_count_posts
-						$totalPosts = wp_count_posts()->publish;
-						echo "<h3>Every artcile I've ever written for this site</h3>";
-						echo "<p>That's a total of <strong>$totalPosts posts</strong> and counting!</p><p>Some of these are longer articles, some are only single pictures. The most recent post is at the top of this list. Explore at your own risk!</p>";
-						$args = array(
-							'type'            => 'postbypost',
-							'limit'           => '',
-							'format'          => 'html', 
-							'before'          => '',
-							'after'           => '',
-							'show_post_count' => false,
-							'echo'            => 1,
-							'order'           => 'DESC'
-						);
-						wp_get_archives( $args ); 
-						// end of all articles 
-						
-						?>
-                        
-						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+					<?php the_content(); ?>
+                    
+                    <?php 
+                    // show a list of all articles
+                    // https://codex.wordpress.org/Function_Reference/wp_count_posts
+                    $totalPosts = wp_count_posts()->publish;
+                    echo "<h3>Annual Summary</h3>";
+                    echo "<p>I've written a total of  <strong>$totalPosts articles</strong> for this site and counting!<br> Here's a list of how many articles I've written per year:</p>";
+                    
+                    // show annual post archives
+                    // https://codex.wordpress.org/Template_Tags/wp_get_archives
+                    $args = array(
+                        'type'            => 'yearly',
+                        'limit'           => '',
+                        'format'          => 'html', 
+                        'before'          => '',
+                        'after'           => '',
+                        'show_post_count' => true,
+                        'echo'            => 1,
+                        'order'           => 'DESC'
+                    );
+                    wp_get_archives( $args ); 
+                    // end of annual archives 
+                    
+                    echo "<h3>Every article I've ever written for this site</h3>";
+                    
+                    // mention word count
+                    $postWordCount = number_format_i18n (guru_getWordCountFromPosts());
+                    $commentWordCount = number_format_i18n (guru_getWordCountCommentsCurrentUser());
+                    $combinedWordCount = number_format_i18n (guru_getWordCountFromPosts()+guru_getWordCountCommentsCurrentUser());
+                    echo "<p>In all these $totalPosts articles, I've written a whopping <strong>$postWordCount</strong> words on this site alone, not including the <strong>$commentWordCount</strong> that I've written as comments in response to my readers.</p>";
+                    echo "<p>Combined together, that's a staggering <strong>$combinedWordCount words!</strong> <br>By comparison, an average paperback contains about 50,000 words.</p>";
+                    
+                    $args = array(
+                        'type'            => 'postbypost',
+                        'limit'           => '',
+                        'format'          => 'html', 
+                        'before'          => '',
+                        'after'           => '',
+                        'show_post_count' => false,
+                        'echo'            => 1,
+                        'order'           => 'DESC'
+                    );
+                    wp_get_archives( $args ); 
+                    // end of all articles  
+                    
+                    ?>
+                    
+                    <?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
 					</div><!-- .entry-content -->
 
 					<footer class="entry-meta">
