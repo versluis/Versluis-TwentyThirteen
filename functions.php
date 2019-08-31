0588365f81cb744d8d4cd40a0118625dbc984c2c
Jay Versluis <versluis2000@yahoo.com>
2019-08-30 20:18:59 -0400

    implemented Divi Affiliate Banner

--- functions.php
+++ functions.php
@@ -44,13 +44,31 @@ function guru_getWordCountCommentsCurrentUser() {
 	return $count;
 }
 
-/* remove JetPack upsell messages */
-/* https://mattreport.com/disable-jetpack-upsell-ads/ */
+//
+// remove JetPack upsell messages 
+// https://mattreport.com/disable-jetpack-upsell-ads/ 
 add_filter( 'jetpack_just_in_time_msgs', '__return_false' );
 
+
+//
 // add Amazon oneLink shenanigans
 function guru_amazonOneLink() {
 	$oneLink = '<div id="amzn-assoc-ad-5e4fb363-4081-411a-8049-667d48d2b006"></div><script async src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=5e4fb363-4081-411a-8049-667d48d2b006"></script>';
 	echo $oneLink;
 }
 add_action ('wp_footer', 'guru_amazonOneLink');
+
+//
+// add Divi Banner at the top of the page
+// @since 2.0
+function showDiviBanner () {
+	// build the banner
+	$divi = '<a href="https://www.elegantthemes.com/affiliates/idevaffiliate.php?id=6674_5_1_20" target="_blank" rel="nofollow"><img style="border:0px" src="https://www.elegantthemes.com/affiliates/media/banners/divi_728x90.jpg" width="728" height="90" alt="Divi WordPress Theme"></a>';
+	$banner = '<div align="center">' . $divi . '</div>';
+	
+	// if we're logged in, don't show the banner
+	if (!is_user_logged_in()) {
+		echo $banner;
+	}
+}
+add_action ('get_header', 'showDiviBanner');
