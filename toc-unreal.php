a4a340105f68468c4160dfa637066556f55332ba
Jay Versluis <versluis2000@yahoo.com>
2020-08-01 11:23:06 -0400

    Create toc-unreal.php

---
+++ toc-unreal.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that other
+ * 'pages' on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ 
+  Template Name: TOC - Unreal Articles
+ 
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<?php /* The loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+						<div class="entry-thumbnail">
+							<?php the_post_thumbnail(); ?>
+						</div>
+						<?php endif; ?>
+
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content">
+						<?php // the_content(); ?>
+                        
+<?php 
+//
+// show a list of articles by tag
+// https://codex.wordpress.org/Class_Reference/WP_Query
+// 
+						
+// grab all articles with tag UNreal Engine
+$query = new WP_Query( array( 'tag' => 'unreal', 'nopaging' => true ) );
+$results = $query->found_posts;
+echo "<p>So far I've written <strong>$results articles</strong> about Unreal Engine 4.<br>Here's a list of each and every one of them:</p>";
+
+// list all articles
+if ($query->have_posts() ) {
+	echo "";
+	while ($query->have_posts() ) {
+		$query->the_post();
+		echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+	}
+	echo "<br><hr>";
+} // end of category articles list
+						
+
+
+
+?>
+                        
+						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+					</div><!-- .entry-content -->
+
+					<footer class="entry-meta">
+						<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
+					</footer><!-- .entry-meta -->
+				</article><!-- #post -->
+
+				<?php // comments_template(); ?>
+			<?php endwhile; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
